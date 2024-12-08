5cab50e670ac3756342a609c9c5c2b8215ed31f8
LIBAZE <507811058@qq.com>
2024-12-08 12:48:36 +0800

    优化训练函数

--- BERTForQA/train.py
+++ BERTForQA/train.py
@@ -26,36 +26,37 @@ def train_model(model, train_dataloader, vaild_dataset, valid_dataloader, optimi
             loss = out.loss
             loss.backward()
             optimizer.step()
+            scheduler.step()
             total_loss += loss.item()
         print("Epoch: {}/{}, Loss: {:.4f}".format(epoch+1, epochs, total_loss / len(train_dataloader)))
         avg, f1, em = evaluate_model(model, vaild_dataset, valid_dataloader)
         print("AVG: {:.4f}, F1: {:.4f}, EM: {:.4f}".format(avg, f1, em))
-        scheduler.step()
+
         print('current learning rate: ', scheduler.get_last_lr())
         if f1 > best_f1:
             best_f1 = f1
-            torch.save(model.state_dict(), './bert_qa_model1.pth')
+            torch.save(model.state_dict(), './bert_qa_model.pth')
             print("Best model saved!")
+        dataset = BertForQADataset('./data/cmrc2018', split='test')
+        dataloader = DataLoader(dataset, batch_size=32, collate_fn=collate_fn)
+        evaluate_model(model, dataset, dataloader)
 
 
 if __name__ == '__main__':
     epochs = 5
     batch_size = 16
-    learning_rate = 5e-5
+    learning_rate = 1e-4
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    T_max, eta_min = epochs, 0
     model = BertForQAModel(num_labels=2).to(device)
-    optimizer = AdamW(model.parameters(), lr=learning_rate)
-    scheduler = CosineAnnealingLR(optimizer, T_max, eta_min)
     train_dataset = BertForQADataset('./data/cmrc2018', split='train')
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
     vaild_dataset = BertForQADataset('./data/cmrc2018', split='validation')
-    valid_dataloader = DataLoader(vaild_dataset, batch_size=batch_size, collate_fn=collate_fn)
+    valid_dataloader = DataLoader(vaild_dataset, batch_size=batch_size*2, collate_fn=collate_fn)
+
+    optimizer = AdamW(model.parameters(), lr=learning_rate)
+    T_max, eta_min = epochs * len(train_dataloader), 1e-7
+    scheduler = CosineAnnealingLR(optimizer, T_max, eta_min)
 
     train_model(model, train_dataloader, vaild_dataset, valid_dataloader, optimizer, scheduler)
-
-
-
-
 
